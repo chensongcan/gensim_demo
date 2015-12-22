e99a50f3f641c8961f154fb83b1db6d31b214132
wjfwzzc <wjfwzzc@gmail.com>
2015-12-22 15:36:32 +0800

    fix bugs

--- train_model/logger/log_conf.py
+++ train_model/logger/log_conf.py
@@ -7,10 +7,10 @@ import logging
 
 # 脚本日志设置
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s:%(msecs)05.1f pid:%(process)d [%(levelname)s] (%(funcName)s) %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    filename='RecSys.log',
-    filemode='a+')
+        level=logging.INFO,
+        format='%(asctime)s:%(msecs)05.1f pid:%(process)d [%(levelname)s] (%(funcName)s) %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filename='train_model.log',
+        filemode='a+')
 
 logger = logging.getLogger()
