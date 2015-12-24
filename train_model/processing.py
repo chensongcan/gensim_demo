5dc3d8588b410318c386de5b5eae3246cdc9988c
wjfwzzc <wjfwzzc@gmail.com>
2015-12-24 14:46:03 +0800

    fix bugs

--- train_model/processing.py
+++ train_model/processing.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 import json
+import os
 import re
 import gensim
 import jieba
@@ -15,7 +16,7 @@ import util
 
 def init(jieba_parallel=False, gensim_warning=False):
     """
-    配置中文分词设置
+    初始化
     :type jieba_parallel: bool
     :type gensim_warning: bool
     :rtype: NoneType
@@ -28,29 +29,9 @@ def init(jieba_parallel=False, gensim_warning=False):
     logger.log.info("module [processing] initialized.")
 
 
-def text_process():
+def _clean_texts():
     """
-    处理position的文字信息
-    :rtype: NoneType
-    """
-    with open("./tmp/pos_data.txt", "w") as f:
-        cnt = 0
-        for row in _clean_content():
-            f.write(json.dumps(row, encoding="utf-8") + "\n")
-            cnt += 1
-            if cnt % 1000 == 0:
-                logger.log.info("texts is saving... number: %s." % (cnt,))
-
-    logger.log.info("texts was saved on disk.")
-
-    util.topic_model.build_lda()
-
-    logger.log.info("texts was processed.")
-
-
-def _clean_content():
-    """
-    文本清洗
+    清洗文本
     :rtype: __generator
     """
     # 过滤空白字符
@@ -62,3 +43,34 @@ def _clean_content():
         # 过滤停用词
         text_filtered = [word for word in text_splited if word not in data.stopwords]
         yield text_filtered
+
+
+def _save_texts(path):
+    """
+    保存文本
+    :type path: str or unicode
+    :rtype: None
+    """
+    with open(path, "w") as f:
+        cnt = 0
+        for row in _clean_texts():
+            f.write(json.dumps(row, encoding="utf-8") + "\n")
+
+            cnt += 1
+            if cnt % 1000 == 0:
+                logger.log.info("texts is saving... number: %s." % (cnt,))
+
+    logger.log.info("texts was saved on disk.")
+
+
+def text_process():
+    """
+    处理position的文字信息
+    :rtype: NoneType
+    """
+    if not os.path.exists("tmp/pos_data.txt"):
+        _save_texts("tmp/pos_data.txt")
+
+    util.topic_model.build_lda()
+
+    logger.log.info("texts was processed.")
--- train_model/util/topic_model.py
+++ train_model/util/topic_model.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 import json
+import os
 import gensim
 import logger
 
@@ -12,60 +13,64 @@ def _get_doc_col():
     """
     :rtype: __generator
     """
-    cnt = 0
-    for row in open("./tmp/pos_data.txt", "r"):
+    for cnt, row in enumerate(open("tmp/pos_data.txt", "rU")):
         yield json.loads(row[:-1], encoding="utf-8")
-        cnt += 1
+
         if cnt % 1000 == 0:
             logger.log.info("texts is being read... number: %s." % (cnt,))
 
 
 def _get_corpus(dictionary):
     """
+    :type dictionary: gensim.corpora.Dictionary
     :rtype: __generator
     """
-    cnt = 0
     for text in _get_doc_col():
         yield dictionary.doc2bow(text)
-        cnt += 1
-        if cnt % 1000 == 0:
-            logger.log.info("corpus is saving... number: %s." % (cnt,))
 
 
-def _build_dictionary():
+def _build_dictionary(path):
     """
     建立词袋
+    :type path: str or unicode
     :rtype: gensim.corpora.Dictionary
     """
-    dictionary = gensim.corpora.Dictionary(_get_doc_col(), prune_at=None)
-    dictionary.save("./tmp/position.dict")
+    if os.path.exists(path):
+        dictionary = gensim.corpora.Dictionary.load(path)
+    else:
+        dictionary = gensim.corpora.Dictionary(_get_doc_col(), prune_at=None)
+        dictionary.save(path)
 
     logger.log.info("dictionary was saved on disk.")
     return dictionary
 
 
-def _build_corpus(dictionary):
+def _build_corpus(dictionary, path):
     """
     建立用词频表示的文档向量
     :type dictionary: gensim.corpora.Dictionary
+    :type path: str or unicode
     :rtype: gensim.corpora.MmCorpus
     """
-    gensim.corpora.MmCorpus.serialize("./tmp/pos_corpus.mm", _get_corpus(dictionary))
-    corpus = gensim.corpora.MmCorpus("./tmp/pos_corpus.mm")
+    if not os.path.exists(path):
+        gensim.corpora.MmCorpus.serialize("tmp/pos_corpus.mm", _get_corpus(dictionary))
+    corpus = gensim.corpora.MmCorpus("tmp/pos_corpus.mm")
 
     logger.log.info("corpus was saved on disk.")
     return corpus
 
 
-def _build_lda(corpus):
+def _build_lda(corpus, path):
     """
     建立LDA模型
     :type corpus: gensim.corpora.MmCorpus
+    :type path: str or unicode
     :rtype: gensim.models.LdaMulticore
     """
     # 建立LDA模型
-    lda = gensim.models.LdaMulticore(corpus, num_topics=300, passes=200, alpha=50 / 300, eta=0.01)
-    lda.save("./tmp/lda.model")
+    lda = gensim.models.LdaMulticore(corpus, num_topics=300, passes=30, alpha=50 / 300, eta=0.01)
+    lda.save(path)
+
     logger.log.info("lda model was saved on disk.")
     return lda
 
@@ -77,8 +82,8 @@ def build_lda():
     """
     logger.log.info("model start generating...")
 
-    dictionary = _build_dictionary()
-    corpus = _build_corpus(dictionary)
-    _build_lda(corpus)
+    dictionary = _build_dictionary("tmp/position.dict")
+    corpus = _build_corpus(dictionary, "tmp/pos_corpus.mm")
+    _build_lda(corpus, "tmp/lda.model")
 
     logger.log.info("model was generated.")
