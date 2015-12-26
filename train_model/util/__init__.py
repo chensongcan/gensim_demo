727c1e2c5a3078596348a008e0092c81184d2b3a
wjfwzzc <wjfwzzc@gmail.com>
2015-12-26 19:13:17 +0800

    del train_model

--- train_model/util/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import unicode_literals
-from util import text_process
-from util import topic_model
-from util import weight_func
